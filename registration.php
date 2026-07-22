9a9ac195147209c886f9bfd150774e091a67e3bf
tinchodev <amartin.c137@gmail.com>
2026-07-22 17:40:05 -0300

    Add Ex6_DbCompatibility module
    Adds MariaDB 11 / MySQL 8 version pattern support for SqlVersionProvider.

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Ex6. All rights reserved.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Ex6_DbCompatibility',
+    __DIR__
+);
